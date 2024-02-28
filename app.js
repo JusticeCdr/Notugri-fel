8e37a8ccf0879657cf5391ca7d91af629380a2d9
JusticeCdr <abduaxadovd@gmail.com>
2024-02-28 19:21:15 +0500

    notugri fellar

---
+++ app.js
@@ -0,0 +1,556 @@
+const array = [
+  {
+    v1: "Think",
+    text: ["Thought", "Thought"],
+  },
+  {
+    v1: "Catch",
+    text: ["Caught", "Caught"],
+  },
+  {
+    v1: "Bring",
+    text: ["Brought", "Brought"],
+  },
+  {
+    v1: "Buy",
+    text: ["Bought", "Bought"],
+  },
+  {
+    v1: "Fight",
+    text: ["Fought", "Fought"],
+  },
+  {
+    v1: "Teach",
+    text: ["Taught", "Taught"],
+  },
+  {
+    v1: "Seek",
+    text: ["Sought", "Sought"],
+  },
+  {
+    v1: "Dream",
+    text: ["Dreamt", "Dreamt"],
+  },
+  {
+    v1: "Sleep",
+    text: ["Slept ", "Slept "],
+  },
+  {
+    v1: "Keep",
+    text: ["Kept ", "Kept "],
+  },
+  {
+    v1: "Deal",
+    text: ["Dealt ", "Dealt "],
+  },
+  {
+    v1: "Mean",
+    text: ["Meant ", "Meant "],
+  },
+  {
+    v1: "Leave",
+    text: ["Left ", "Left "],
+  },
+  {
+    v1: "Feel",
+    text: ["Felt ", "Felt "],
+  },
+  {
+    v1: "Sweep",
+    text: ["Swept ", "Swept "],
+  },
+  {
+    v1: "Bend",
+    text: ["Bent ", "Bent "],
+  },
+  {
+    v1: "Build",
+    text: ["Built ", "Built "],
+  },
+  {
+    v1: "Lend",
+    text: ["Lent ", "Lent "],
+  },
+  {
+    v1: "Lose",
+    text: ["Lost ", "Lost "],
+  },
+  {
+    v1: "Spend",
+    text: ["Sent ", "Sent "],
+  },
+  {
+    v1: "Send",
+    text: ["Spent ", "Spent "],
+  },
+  {
+    v1: "Stick",
+    text: ["Stuck ", "Stuck "],
+  },
+  {
+    v1: "Dig",
+    text: ["Dug ", "Dug "],
+  },
+  {
+    v1: "Sting",
+    text: ["Stung   ", "Stung   "],
+  },
+
+  {
+    v1: "Swing",
+    text: ["swung", "swung"],
+  },
+
+  {
+    v1: "Hang",
+    text: ["Hung ", "Hung "],
+  },
+
+  {
+    v1: "Strike",
+    text: ["Struck ", "Struck "],
+  },
+
+  {
+    v1: "Cut",
+    text: ["Cut", "Cut"],
+  },
+
+  {
+    v1: "Put",
+    text: ["Put", "Put"],
+  },
+
+  {
+    v1: "Quit",
+    text: ["Quit", "Quit"],
+  },
+
+  {
+    v1: "Bet",
+    text: ["Bet", "Bet"],
+  },
+
+  {
+    v1: "Cost",
+    text: ["Cost", "Cost"],
+  },
+
+  {
+    v1: "Spread",
+    text: ["Spread", "Spread"],
+  },
+
+  {
+    v1: "Let",
+    text: ["Let", "Let"],
+  },
+
+  {
+    v1: "Broadcast",
+    text: ["Broadcast", "Broadcast"],
+  },
+
+  {
+    v1: "Hit",
+    text: ["Hit", "Hit"],
+  },
+
+  {
+    v1: "Hurt",
+    text: ["Hurt", "Hurt"],
+  },
+
+  {
+    v1: "Read",
+    text: ["Read", "Read"],
+  },
+
+  {
+    v1: "Set",
+    text: ["Set", "Set"],
+  },
+
+  {
+    v1: "Shut",
+    text: ["Shut", "Shut"],
+  },
+
+  {
+    v1: "Split",
+    text: ["Split", "Split"],
+  },
+
+  {
+    v1: "Burst",
+    text: ["Burst", "Burst"],
+  },
+
+  {
+    v1: "Fit",
+    text: ["Fit", "Fit"],
+  },
+
+  {
+    v1: "Ring",
+    text: ["Rang ", "Rung "],
+  },
+
+  {
+    v1: "Sing",
+    text: ["Sang ", "Sung "],
+  },
+
+  {
+    v1: "Sink",
+    text: ["Sank ", "Sunk "],
+  },
+
+  {
+    v1: "Drink",
+    text: ["Drank ", "Drunk "],
+  },
+
+  {
+    v1: "Swim",
+    text: ["Swam ", "Swum "],
+  },
+
+  {
+    v1: "Begin",
+    text: ["Began ", "Begun "],
+  },
+
+  {
+    v1: "Run",
+    text: ["Ran ", "Run "],
+  },
+
+  {
+    v1: "Shrink",
+    text: ["Shrank ", "Shrunk "],
+  },
+
+  {
+    v1: "Spring",
+    text: ["Sprang ", "Sprung "],
+  },
+
+  {
+    v1: "Stink",
+    text: ["Stank ", "Stunk "],
+  },
+
+  {
+    v1: "Speak",
+    text: ["Spoke ", "Spoken"],
+  },
+
+  {
+    v1: "Choose",
+    text: ["Chose ", "Chosen "],
+  },
+
+  {
+    v1: "Break",
+    text: ["Broke ", "Broken"],
+  },
+
+  {
+    v1: "Drive",
+    text: ["Drove ", "Driven "],
+  },
+
+  {
+    v1: "Freeze",
+    text: ["Froze ", "Frozen "],
+  },
+
+  {
+    v1: "Steal",
+    text: ["Stole ", "Stolen "],
+  },
+
+  {
+    v1: "Wake",
+    text: ["Woke ", "Woken "],
+  },
+
+  {
+    v1: "Write",
+    text: ["Wrote ", "Written "],
+  },
+
+  {
+    v1: "Rise",
+    text: ["Rose  ", "Risen  "],
+  },
+
+  {
+    v1: "Ride",
+    text: ["Rode", "Ridden "],
+  },
+
+  {
+    v1: "Beat",
+    text: ["Beat ", "Beaten "],
+  },
+
+  {
+    v1: "Bit",
+    text: ["Bit", "Bitten "],
+  },
+
+  {
+    v1: "Hide",
+    text: ["Hid ", "Hidden "],
+  },
+
+  {
+    v1: "Forbid",
+    text: ["Forbade", "Forbidden"],
+  },
+
+  {
+    v1: "Forget",
+    text: ["Forgot ", "Forgotten"],
+  },
+
+  {
+    v1: "Get",
+    text: ["Got ", "Gotten "],
+  },
+
+  {
+    v1: "Fall",
+    text: ["Fell ", "Fallen "],
+  },
+
+  {
+    v1: "Eat",
+    text: ["Ate ", "Eaten "],
+  },
+
+  {
+    v1: "Shake",
+    text: ["Shook ", "Shaken"],
+  },
+
+  {
+    v1: "Take",
+    text: ["Took ", "Taken "],
+  },
+
+  {
+    v1: "Become",
+    text: ["Became", "Become"],
+  },
+
+  {
+    v1: "Come",
+    text: ["Came ", "Come "],
+  },
+
+  {
+    v1: "Blow",
+    text: ["Blew ", "Blown "],
+  },
+
+  {
+    v1: "Draw",
+    text: ["Drew ", "Drawn "],
+  },
+
+  {
+    v1: "Fly",
+    text: ["Flew ", "Flown "],
+  },
+
+  {
+    v1: "Grow",
+    text: ["Grew ", "Grown "],
+  },
+
+  {
+    v1: "Know",
+    text: ["Knew ", "Known "],
+  },
+
+  {
+    v1: "Throw",
+    text: ["Threw ", "Thrown "],
+  },
+
+  {
+    v1: "Show",
+    text: ["Showed", "Shown "],
+  },
+  {
+    v1: "Sew",
+    text: ["Sewed", "Sewn/sewed(syuen/syued)"],
+  },
+
+  {
+    v1: "Feed",
+    text: ["Fed ", "Fed "],
+  },
+
+  {
+    v1: "Find",
+    text: ["Found ", "Found "],
+  },
+
+  {
+    v1: "Have",
+    text: ["Had ", "Had "],
+  },
+
+  {
+    v1: "Hear",
+    text: ["Heard ", "Heard "],
+  },
+
+  {
+    v1: "Hold",
+    text: ["Held ", "Held "],
+  },
+
+  {
+    v1: "Lay",
+    text: ["Laid ", "Laid "],
+  },
+
+  {
+    v1: "Lead",
+    text: ["Led ", "Led "],
+  },
+
+  {
+    v1: "Light",
+    text: ["Lit ", "Lit "],
+  },
+
+  {
+    v1: "Make",
+    text: ["Made ", "Made "],
+  },
+  {
+    v1: "Meet",
+    text: ["Met  ", "Met  "],
+  },
+
+  {
+    v1: "Pay",
+    text: ["Paid  ", "Paid "],
+  },
+
+  {
+    v1: "Say",
+    text: ["Said  ", "Said  "],
+  },
+
+  {
+    v1: "Sell",
+    text: ["Sold  ", "Sold  "],
+  },
+
+  {
+    v1: "Shine",
+    text: ["Shone  ", "Shone"],
+  },
+
+  {
+    v1: "Shoot",
+    text: ["Shot  ", "Shot  "],
+  },
+
+  {
+    v1: "Sit",
+    text: ["Sat  ", "Sat  "],
+  },
+
+  {
+    v1: "Stand",
+    text: ["Stood  ", "Stood  "],
+  },
+
+  {
+    v1: "Tell",
+    text: ["Told  ", "Told  "],
+  },
+
+  {
+    v1: "Understand",
+    text: ["Understood", "Understood"],
+  },
+
+  {
+    v1: "Win",
+    text: ["Won ", "Won "],
+  },
+
+  {
+    v1: "Be",
+    text: ["Was/were", "Been "],
+  },
+
+  {
+    v1: "Do",
+    text: ["Did ", "Done "],
+  },
+
+  {
+    v1: "Go",
+    text: ["Went ", "Gone"],
+  },
+
+  {
+    v1: "See",
+    text: ["Saw ", "Seen "],
+  },
+
+  {
+    v1: "Lie",
+    text: ["Lay ", "Lain "],
+  },
+
+  {
+    v1: "Forgive ",
+    text: ["Forgave", "Forgiven"],
+  },
+
+  {
+    v1: "Give",
+    text: ["Gave ", "Given "],
+  },
+
+  {
+    v1: "Swear",
+    text: ["Swore ", "Sworn "],
+  },
+
+  {
+    v1: "Tear",
+    text: ["Tore ", "Torn "],
+  },
+  {
+    v1: "Wear",
+    text: ["Wore ", "Worn "],
+  },
+];
+
+let input = document.querySelector(".v1");
+const answear1 = document.querySelector(".v2");
+const answear2 = document.querySelector(".v3");
+const searchBtn = document.querySelector(".btn");
+
+searchBtn.addEventListener("click", searchElement);
+
+function searchElement() {
+  array.forEach((arr) => {
+    if (arr.v1 === input.value) {
+      answear1.value = arr.text[0];
+      answear2.value = arr.text[1];
+    }
+  });
+}
